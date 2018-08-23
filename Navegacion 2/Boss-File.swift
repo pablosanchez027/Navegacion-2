0ec34ac36ed7ddbd2e0f802fb2d8c1adef0f8ff3
Maestro <Maestro@iMac-de-Maestro-5.local>
2018-08-23 13:27:31 -0700

    Bug on Nordic Viewport

---
+++ Navegacion 2/Boss-File.swift
@@ -0,0 +1,29 @@
+//
+//  Boss-File.swift
+//  Navegacion 2
+//
+//  Created by Maestro on 23/08/18.
+//  Copyright © 2018 Maestro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+internal class Rusviet :  UIViewController
+{
+    @IBAction func doTapSaxony(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    override internal func viewDidLoad() {
+        
+    }
+}
+
+internal class Nordic : UIViewController
+{
+    @IBAction func doTapSaxony(sender: AnyObject) {
+    }
+    override internal func viewDidLoad() {
+        self.dismissViewControllerAnimated(true, completion: nil)    
+    }
+}
