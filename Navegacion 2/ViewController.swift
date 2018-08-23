7148959f203ea29687418abe06c10d07b8755cc8
Maestro <Maestro@imac-de-maestro-5.local>
2018-08-23 12:14:11 -0700

    Initial Commit

---
+++ Navegacion 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navegacion 2
+//
+//  Created by Maestro on 23/08/18.
+//  Copyright © 2018 Maestro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
